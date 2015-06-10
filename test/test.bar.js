c4b77d808809689634ea2e7e117c4a3e4064c639
Justin Combs <combs.justin.m@gmail.com>
2015-06-09 22:07:21 -0400

    testBar complete

--- test/test.bar.js
+++ test/test.bar.js
@@ -3,12 +3,57 @@
 (function () {
   'use strict';
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
-        expect(1).to.equal(1);
+   describe("Bar", function() {
+      var bar;
+
+    beforeEach(function() {
+      bar = new Bar();
+    });
+
+    describe('square', function() {
+      it('should square a number', function () {
+        expect(bar.squared(2)).to.equal(4);
       });
     });
+
+    describe('cube', function() {
+      it('should cube a number', function () {
+        expect(bar.cubed(3)).to.equal(27);
+      });
+    });
+
+    describe('exponent', function() {
+      it('should output number with given power', function () {
+        var square = bar.exponentOf(2);
+        expect(square(4)).to.equal(16);
+      });
+    });
+
+    describe('Reverse String', function() {
+      it('should output a string in reverse', function () {
+        expect(bar.reverseString('abc')).to.equal('cba');
+      });
+    });
+
+    describe('cipher', function() {
+      it('should cipher a string', function() {
+        expect(bar.cipher('justin')).to.equal('ÎÙ×ØÍÒ');
+      });
+    });
+
+    describe('decipher', function() {
+      it('should decipher a string', function() {
+        expect(bar.decipher('ÎÙ×ØÍÒ')).to.equal('justin');
+      });
+    });
+
+
+    describe('rot13', function() {
+      it('should turn a sentence into a character code string', function() {
+        expect(bar.rot13('justin combs')).to.equal('whfgva pbzof');
+      });
+    });
+
   });
 
 })();
