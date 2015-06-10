28f5cd3eb28cfd164acfb4cec299f870c6d31343
Justin Combs <combs.justin.m@gmail.com>
2015-06-09 20:54:30 -0400

    testFoo completed

--- app/js/foo.js
+++ app/js/foo.js
@@ -1,12 +1,17 @@
 window.Foo = (function() {
 
   function Foo() {
-
+    this.plusOne = function(n){return n + 1;};
+    this.sortArray = function(arr){
+      var clone = arr.slice().sort();
+      return clone};
+    this.hasValue = function(list, item) {
+      return list.indexOf(item) >= 0;};
   }
 
-  Foo.prototype = {
-
-  };
+  Foo.greet = function(val) {
+    return "Hello, " + val + "!";
+    };
 
   return Foo;
 
